49e9083400820e4b8dbd57980bad299f66a32fab
FxnnBT <40134@ma-web.nl>
2026-01-08 12:09:05 +0100

    javascript blokje toevoegen

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const buttonclicked
+const button = document.getElementById('showMessage');
+const message = document.getElementById('message');
+
+button.addEventListener('click', function() {
+    if (message.style.display === 'none') {
+        message.style.display = 'block';
+        button.textContent = 'Verberg bericht';
+    } else {
+        message.style.display = 'none';
+        button.textContent = 'Klik hier!';
+    }
+});
